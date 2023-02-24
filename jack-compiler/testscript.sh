4452d16002532e8a783710f5142ee8a7d146074f
Micheal Nestor <michealnestor@outlook.com>
2023-02-24 17:35:28 +0000

    updated the testscript inline with the new directory structure

--- jack-compiler/testscript.sh
+++ jack-compiler/testscript.sh
@@ -6,9 +6,15 @@ jackFiles=(Main.jack Ball.jack Empty.jack EofInComment.jack EofInStr.jack Fracti
 tokenFiles=(Main.jack_tokens.txt Ball.jack_tokens.txt Empty.jack_tokens.txt EofInComment.jack_tokens.txt EofInStr.jack_tokens.txt Fraction.jack_tokens.txt IllegalSymbol.jack_tokens.txt List.jack_tokens.txt NewLineInStr.jack_tokens.txt OnlyComments.jack_tokens.txt Square.jack_tokens.txt SquareGame.jack_tokens.txt)
 
 clear
+# build the program
 printf "Building lexer\n"
 gcc lexer.c -o lexer
+# move into the test directory and bring the executable
+mv lexer ./testfiles/
+cd ./testfiles
 index=0
+# run each of the jack files through the executable and 
+# diff with the corresponding token files
 for file in ${jackFiles[@]}; do
     printf "Testing with $file\n"
     $(./lexer $file output)
@@ -24,4 +30,6 @@ for file in ${jackFiles[@]}; do
     fi
     index=$((index + 1))
 done
+# remove the executable and the output file and move back
 rm lexer output
+cd ..
